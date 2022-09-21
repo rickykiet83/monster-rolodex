790fe51cc662f735e46365c4f559713dbfc6b35a
Kiet Pham <kietpham.dev@gmail.com>
2022-09-21 20:53:03 +1000

    Finishing touches

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,23 +2,15 @@ import './card-list.styles.css';
 
 import React, { Component } from 'react';
 
+import Card from '../card/card.component';
+
 export default class CardList extends Component {
   render() {
     const { monsters } = this.props;
     return (
       <div className='card-list'>
         {monsters.map((monster) => {
-          const { name, id, email } = monster;
-          return (
-            <div className='card-container' key={id}>
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set3&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
