e69610216e2d7af2509e1069fcfbb4779396421e
Kiet Pham <kiet.pham2610@gmail.com>
2023-02-04 17:32:13 +1100

    Hooks: useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import logo from './logo.svg';
-import { useState } from 'react';
 
 const App = () => {
 	const [searchField, setSearchField] = useState('');
-	console.log({ searchField });
+	const [monsters, setMonsters] = useState([]);
+	const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users').then((response) =>
+			response.json().then((users) => setMonsters(users))
+		);
+	}, []);
+
+	useEffect(() => {
+		const newFilteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField)
+		);
+
+		setFilterMonsters(newFilteredMonsters);
+	}, [monsters, searchField]);
 
 	const onSearchChange = (event) => {
 		const searchFieldString = event.target.value.toLowerCase();
@@ -22,7 +38,7 @@ const App = () => {
 				placeholder='search monsters'
 				className='monsters-search-box'
 			/>
-			{/* <CardList monsters={filteredMonsters} /> */}
+			<CardList monsters={filteredMonsters} />
 		</div>
 	);
 };
